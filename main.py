91c96c8deecf3863af76aa4476c607c9e20fb3df
Daniel Ayers <danielayers2019@gmail.com>
2021-12-01 11:23:05 -0700

    works with video

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import cv2
 import numpy as np
+import sys
 
 # constants
-IMAGE_COUNT = 5
 MAX_WIDTH = 700
+MAX_HEIGHT = 700
 OUTPUT_FRAME_SIZE = 400
 
 thresh = 0.60
-num_thresh = 0.85
+num_thresh = 0.8
 connectivity = 4
 template = cv2.imread('train/eyvel_win.PNG')
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
+found_prev_frame = False
+
+# # get camera feed
+# cap = cv2.VideoCapture(2)
+# if not cap.isOpened():
+#     print("cannot open camera")
+#     exit()
+
+video_capture = cv2.VideoCapture("gameplay.mp4")     # Open video capture object
+got_image, bgr_image = video_capture.read()       # Make sure we can read video
+if not got_image:
+    print("Cannot read video source")
+    sys.exit()
 
 # reshapes the output image
 def reshape(img):
     if img.shape[1] > MAX_WIDTH:
         s = MAX_WIDTH / img.shape[1]
         img = cv2.resize(img, dsize=None, fx=s, fy=s)
+    elif img.shape[0] > MAX_HEIGHT:
+        s = MAX_HEIGHT / img.shape[0]
+        img = cv2.resize(img, dsize=None, fx=s, fy=s)
 
     return img
+
+def reshape_for_template(img):
+    return cv2.resize(img, (730, 1870))
 
 # takes numbers from template matching step, maps them to 2 digit nums
 def getValuesFromNumbers(inputs):
@@ -131,41 +152,96 @@ def calcChances(stats):
     cv2.putText(chance_img, death_msg,org=(10, 45), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(0,255,255))
     cv2.imshow("chance", chance_img)
 
-# list of numbers found in the image with their (x, y) coordinates
-for i in range(1, IMAGE_COUNT + 1):
+
+while True:
+    # # get video feed
+    # ret, bgr_image = cap.read()
+    #
+    # if not ret:
+    #     print("Can't receive stream")
+    #     break
+
+    got_image, bgr_image = video_capture.read()
+    if not got_image:
+        break  # End of video; exit the while loop
+
+    gray = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
 
     # read in query image
-    query_img = cv2.imread('screen/screen' + str(i) + '.PNG')
-    bgr_image_output = query_img.copy()
+    query_img = gray
+    bgr_image_output = bgr_image
 
-    # score the image to find the object
-    scores = cv2.matchTemplate(query_img, template, cv2.TM_CCOEFF_NORMED)
+    # # do feature matching for video
+    # orb = cv2.ORB_create(nfeatures=10000)
+    # trainKeypoints, trainDesc = orb.detectAndCompute(template, None)
+    # queryKeypoints, queryDesc = orb.detectAndCompute(query_img, None)
+    #
+    # matcher = cv2.BFMatcher.create(cv2.NORM_L2)
+    # matches = matcher.knnMatch(queryDesc, trainDesc, k=2)
+    # good = []
+    # for m, n in matches:
+    #     if m.distance < 0.7 * n.distance:
+    #         good.append(m)
+    # matches = good
+    # print("Number of raw matches between training and query:", len(matches))
+    #
+    # bgr_matches = cv2.drawMatches(
+    #     img1=query_img, keypoints1=queryKeypoints,
+    #     img2=template, keypoints2=trainKeypoints,
+    #     matches1to2=matches, matchesMask=None, outImg=None
+    # )
+    # cv2.imshow("all matches", bgr_matches)
+    # cv2.waitKey(0)
+    #
+    # if len(matches) >= 3:
+    #     # Estimate affine transformation from training to query image points.
+    #     # Use the "least median of squares" method for robustness. It also detects outliers.
+    #     # Outliers are those points that have a large error relative to the median of errors.
+    #     src_pts = np.float32([trainKeypoints[m.trainIdx].pt for m in matches]).reshape(
+    #         -1, 1, 2)
+    #     dst_pts = np.float32([queryKeypoints[m.queryIdx].pt for m in matches]).reshape(
+    #         -1, 1, 2)
+    #     A_train_query, inliers = cv2.estimateAffine2D(
+    #         dst_pts, src_pts,
+    #         method=cv2.LMEDS)
+    #
+    # # Apply the affine warp to warp the training image to the query image.
+    # if A_train_query is not None and len(inliers) >= 3:
+    #     # Object detected! Warp the training image to the query image and blend the images.
+    #     print("Object detected! Found %d inlier matches" % sum(inliers))
+    #     warped_image = cv2.warpAffine(
+    #         src=query_img, M=A_train_query,
+    #         dsize=(template.shape[1], template.shape[0]))
+    #
+    #     cv2.imshow("match", warped_image.astype(np.uint8))
+    #     cv2.waitKey(0)
+
+    # score the image to find the battle forecast
+    scores = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(scores)
-    max_score = -1
 
-    if(max_val > max_score):
-        max_score = max_val
 
-    # set the location of the matched object
-    topLeft = max_loc
-    bottomRight = (topLeft[0] + (template.shape[1]), topLeft[1] + (template.shape[0]))
-    forecast_nums = []
+    # if match exceeds threshold, we found the forecast
+    if(max_val > thresh) and not found_prev_frame:
+        found_prev_frame = True
 
-    # if match exceeds threshold, we have a good match
-    if(max_val > thresh):
-        cv2.rectangle(bgr_image_output, topLeft, bottomRight, (0, 0, 255), 2)
+        # set the location of the matched object
+        topLeft = max_loc
+        bottomRight = (topLeft[0] + (template.shape[1]), topLeft[1] + (template.shape[0]))
+        forecast_nums = []
 
         # extract the part of the image containing the battle forecast
         subset_img = query_img[topLeft[1]:bottomRight[1], topLeft[0]:bottomRight[0]]
+        subset_img = reshape_for_template(subset_img)
 
         for i in range(0, 10):
             # find numbers within the forecast
             num = cv2.imread('nums/r' + str(i) + '.PNG')
 
-            gray_template = cv2.cvtColor(num, cv2.COLOR_BGR2GRAY)
-            gray_img = cv2.cvtColor(subset_img, cv2.COLOR_BGR2GRAY)
+            gray_num = cv2.cvtColor(num, cv2.COLOR_BGR2GRAY)
+            subset_copy = subset_img.copy()
 
-            scores = cv2.matchTemplate(gray_img, gray_template, cv2.TM_CCOEFF_NORMED)
+            scores = cv2.matchTemplate(subset_img, gray_num, cv2.TM_CCOEFF_NORMED)
 
             # Threshold the image and extract centroids
             thresholded = (scores > num_thresh).astype(np.uint8)
@@ -175,12 +251,17 @@ for i in range(1, IMAGE_COUNT + 1):
             # Throw out the background centroid
             centroids = centroids[1:]
 
-            # Draw rectangles around each letter
+            # Draw rectangles around each number
             for centroid in centroids:
                 forecast_nums.append([int(centroid[0]), int(centroid[1]), i])
+                cv2.rectangle(subset_copy,
+                              (int(centroid[0]), int(centroid[1]), int(num.shape[1]), int(num.shape[0])),
+                              color=(0,0,255), thickness=4)
+
 
         dash = cv2.imread('nums/na.PNG')
-        scores = cv2.matchTemplate(query_img, dash, cv2.TM_CCOEFF_NORMED)
+        dash = cv2.cvtColor(dash, cv2.COLOR_BGR2GRAY)
+        scores = cv2.matchTemplate(subset_img, dash, cv2.TM_CCOEFF_NORMED)
 
         # Threshold the image and extract centroids
         thresholded = (scores > num_thresh).astype(np.uint8)
@@ -199,12 +280,16 @@ for i in range(1, IMAGE_COUNT + 1):
 
         # calculate chances of killing/being killed
         calcChances(stats)
+    elif max_val > thresh:
+        found_prev_frame = True
     else:
         # set display to be blank
         chance_img = np.ones((int(OUTPUT_FRAME_SIZE * .15), OUTPUT_FRAME_SIZE))
         cv2.imshow("chance", chance_img)
+        found_prev_frame = False
 
     # output image to screen
     bgr_image_output = reshape(bgr_image_output)
     cv2.imshow("Output", bgr_image_output)
-    cv2.waitKey(0)
+    if cv2.waitKey(1) == ord('q'):
+        break
